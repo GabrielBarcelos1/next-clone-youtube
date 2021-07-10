2a2077c07b51ccdab94fd19ebdcdfa7e8264de51
gabriel barcelos <gabrielvieirabarcelos02@gmail.com>
2021-07-10 13:14:46 +0000

    adding material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       clone youtube nextJs
+      <Button variant="outlined" color="secondary">teste</Button>
     </div>
   );
 }
